17f9490532674a9fee77b7907659009760c3800e
이성준 <qoik11@naver.com>
2019-09-27 11:34:31 +0900

    탭 구현 에뮬레이터에 탭바텀 부분이 안나옴

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppLoading } from "expo";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
+import TabNavigation from "./navigation/TabNavigation";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    loaded: false
+  };
+
+  handleError = error => console.log(error);
+
+  handleLoaded = () => this.setState({ loaded: true });
+
+  loadAssets = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+  };
+
+  render() {
+    const { loaded } = this.state;
+    if (loaded) {
+      return <TabNavigation />;
+    } else {
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={this.handleLoaded}
+          onError={this.handleError}
+        />
+      );
+    }
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
